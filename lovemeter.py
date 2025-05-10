5b18c70c1d15c20f72f477aea6c6abf41fa93dda
JustLorenzoo <lorenzozhian@gmail.com>
2025-05-10 14:21:57 +0700

    lovemeter.py

--- lovemeter.py
+++ lovemeter.py
@@ -5,7 +5,7 @@ import random
 print("Love meter")
 a = input("Masukkan nama 1: ")
 b = input("Masukkan nama 2: ")
-c = random.randint(80,100)
+c = random.randint(50,100)
 
 if b.strip():
     print(f"Kecocokan dari {a} Love {b} Adalah {c}%")
